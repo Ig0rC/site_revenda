5b223bbd040159ec1299447ea5fbda3db77c2011
Igor Cardoso da Silva <igorcardosod3@gmail.com>
2020-11-18 15:58:31 -0300

    15:59 18:11

--- js/lotties.js
+++
@@ -1,7 +0,0 @@
-var animation = bodymovin.loadAnimation({
-    container: document.getElementById('ani-one'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: '/lf30_editor_degqqnad.json'
-})
